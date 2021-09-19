06fd867f5e7116bc1ea6815697bd62a71364a466
alphine06 <91019724+alphine06@users.noreply.github.com>
2021-09-19 21:44:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'em yêu đấy à!',
+    introDesc: `em có biết là anh yêu em nhiều lắm hong, to lắm lun ó, to không đếm nổi lun ó, Màu anh thích là màu xanh dương, còn em mãi là người anh thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'mít yêu của anemari 😙',
+    desc: 'đỗ thị mỹ Duyên có yêu anh hong',
+    btnYes: 'yêu nhất quả đất lun ó',
+    btnNo: 'Không nha',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'i love you',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'sớm lên vs anh nha',
+    btnAccept: 'Okiiiii darling',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
